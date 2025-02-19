e85cdd8fb886da521d47f25e95e4391c77b58852
Лиза <elisavetamoto@gmail.com>
2025-02-19 22:38:50 +0300

    add variables sum, diff, mult, quotient

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        //введение чисел с консоли
+
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        //объявление переменных для результата операций
+        int sum = firstNumber + secondNumber;
+        int diff = firstNumber - secondNumber;
+        int mult = firstNumber * secondNumber;
+        double quotient = (double) firstNumber/secondNumber;
+
+        //вывод результатов операций
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + diff);
+        System.out.println("Произведение: " + mult);
+        System.out.println("Частное: " + quotient);
 
     }
 }
