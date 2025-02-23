3bd9f939685e7b42787b4e1f997e714eea46b111
Лиза <elisavetamoto@gmail.com>
2025-02-24 01:08:45 +0300

    add checking file path

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,38 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int fileCount = 0; // Счетчик верно указанных файлов
 
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = scanner.nextLine();
 
+            File file = new File(path);
+
+            // Проверяем существует ли файл
+            boolean fileExists = file.exists();
+
+            // Проверяем является ли путь файлом
+            boolean isFile = file.isFile();
+
+            // Проверяем условия
+            if (!fileExists) {
+                System.out.println("Файл не существует. Попробуйте снова.");
+                continue;
+            }
+
+            if (!isFile) {
+                System.out.println("Путь ведет к папке, а не к файлу. Попробуйте снова.");
+                continue;
+            }
+
+            // Если путь указан верно
+            System.out.println("Путь указан верно.");
+            fileCount++;
+            System.out.println("Это файл номер " + fileCount);
+        }
     }
 }
